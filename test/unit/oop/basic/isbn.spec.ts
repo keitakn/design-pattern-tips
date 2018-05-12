77f5cf6b167f413fa85da91285ff6bdc30fcd84e
keitakn <keita.koga.work@gmail.com>
2018-05-12 19:56:02 +0900

    add #1 書籍のISBNクラスを作成しテストコードを追加

---
+++ test/unit/oop/basic/isbn.spec.ts
@@ -0,0 +1,25 @@
+import Isbn from '../../../../src/oop/basic/Isbn';
+
+/**
+ * ISBNクラスのテスト
+ */
+describe('Isbn', () => {
+
+  /**
+   * 言語コードを取り出せる事を担保する
+   */
+  it('should be able to extract the language code', () => {
+    const isbnJapan = new Isbn('978-4-09-825202-2');
+
+    expect(isbnJapan.extractLanguage()).toBe('ja');
+
+    let isbnEnglish = new Isbn('978-1-40-127734-5');
+    expect(isbnEnglish.extractLanguage()).toBe('en');
+
+    isbnEnglish = new Isbn('978-0-40-127734-5');
+    expect(isbnEnglish.extractLanguage()).toBe('en');
+
+    const isbnFrance = new Isbn('978-2-10-825202-2');
+    expect(isbnFrance.extractLanguage()).toBe('fr');
+  });
+});
